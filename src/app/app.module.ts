60d2979017ca24e94271e4cf3686b629b6db1f15
Ssensamba Allan <ssensambaallan66@gmail.com>
2022-07-08 17:09:52 +0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,CoursesComponent, TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,6 @@
+export class CoursesService{
+
+    getCourses(){
+        return["ALLAN","KATO","WASSWA","EMMA"];
+    }
+}
